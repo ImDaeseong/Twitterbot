aabd1cbd4c00136234d8783ff98392378e7f1186
ImDaeseong <cs93059@gmail.com>
2019-12-18 10:09:54 +0900

    Twitterbot

--- bot/twitterbot.py
+++ bot/twitterbot.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+from datetime import datetime
 from tweepy import TweepError
 
 
@@ -75,20 +76,54 @@ def searchCursor(api, skey):
     return result
 
 
+def searchretweet(api, skey):
+    try:
+        for tweet in tweepy.Cursor(api.search, q=skey).items(10):
+            print('\n@' + tweet.user.screen_name + ' - ' + tweet.text)
+            tweet.retweet()
+            time.sleep(10)
+    except tweepy.TweepError as error:
+        print(error.reason)
+
+
+def checkfavorite(api, skey):
+    try:
+        for tweet in tweepy.Cursor(api.search, q=skey).items():
+            print('\n@' + tweet.user.screen_name + ' - ' + tweet.text)
+            # 좋아요 누르기
+            tweet.favorite()
+            # follow 하기
+            if not tweet.user.following:
+               tweet.user.follow()
+
+    except tweepy.TweepError as error:
+        print(error.reason)
+
+def photopath(api, path):
+    try:
+        now = datetime.now()
+        status = 'my photo:' +  now.strftime("%Y-%m-%d %H:%M:%S")
+        api.update_with_media(path, status=status)
+    except tweepy.TweepError as error:
+        print(error.reason)
+
+
 if __name__ == "__main__":
     api = getAPI()
     user = api.me()
 
-    """
-    print(user.friends_count)
-    print(user.description)
-    print(user.location)
-    print(user.id)
-    print(user.id_str)
-    print(user.screen_name)
-    print(user.name)
-    print(api.followers_ids())
-    """
+    # api.update_status(status="공부중")
+
+    print('user.friends_count:', user.friends_count)
+    print('user.description:', user.description)
+    print('user.location:', user.location)
+    print('user.id:', user.id)
+    print('user.id_str:', user.id_str)
+    print('user.screen_name:', user.screen_name)
+    print('user.name:', user.name)
+    print('user.followers_count:', user.followers_count)
+    print('user.statuses_count:', str(user.statuses_count))
+    print('user.time_zone:', user.time_zone)
 
     # api.update_status("@" + user.screen_name, in_reply_to_status_id=user.id)
     # hometimeline(api)
@@ -102,6 +137,15 @@ if __name__ == "__main__":
         print(result)
     """
 
+    """
     results = searchKeyword(api, '최신 영화')
     for result in results:
         print(result)
+    """
+
+    # searchretweet(api, '최신 영화')
+    # checkfavorite(api, '#python')
+    # photopath(api, 'e:/a.jpg')
+
+
+
